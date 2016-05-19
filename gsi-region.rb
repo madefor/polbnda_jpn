781608a93aa25bead0ea2145dd09f91a528381f8
Hidenori FUJIMURA <hfu@si.local>
2016-05-19 21:23:38 +0900

    update

--- gsi-region.rb
+++ gsi-region.rb
@@ -4,31 +4,31 @@ r = JSON.parse(File.open('polbnda_jpn.geojson').read)
 r['features'].each{|f|
   %w{f_code coc nam laa pop ypc salb soc}.each{|k| f['properties'].delete(k)}
   pref = f['properties']['adm_code'].to_i / 1000
-  fill = '#555'
+  color = '#555'
   case pref
   when 1
-    fill = '#f7f'
+    color = '#f7f'
   when 2..7
-    fill = '#0f7'
+    color = '#0f7'
   when 8..14, 19..20
-    fill = '#f73'
+    color = '#f73'
   when 15..18
-    fill = '#77f'
+    color = '#77f'
   when 21..24
-    fill = '#caf'
+    color = '#caf'
   when 25..30
-    fill = '#ca4'
+    color = '#ca4'
   when 31..35
-    fill = '#cf0'
+    color = '#cf0'
   when 36..39
-    fill = '#c70'
+    color = '#c70'
   when 40..46
-    fill = '#c04'
+    color = '#c04'
   when 47
-    fill = '#474'
+    color = '#474'
   end
-  f['properties']['fill'] = fill
-  f['properties']['stroke-width'] = 0
-  f['properties']['fill-opacity'] = 0.8
+  f['properties']['fill'] = color
+  f['properties']['stroke'] = color
+  f['properties']['fill-opacity'] = 0.6
 }
 File.open('gsi-region.geojson', 'w'){|w| w.print JSON.dump(r)}
